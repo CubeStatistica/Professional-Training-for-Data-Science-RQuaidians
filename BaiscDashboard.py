043ceab7315922de51adee05776ed8c1cb57043a
javariaqayyum <97040671+javariaqayyum@users.noreply.github.com>
2023-02-22 14:31:29 +0500

    Added more visulas.

--- BaiscDashboard.py
+++ BaiscDashboard.py
@@ -9,12 +9,18 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True)
 data = pd.read_csv("Data/Data - DS C1 Course.csv")
 
 fig = px.bar(data, x="Age", y="Are you currently attending University / College?", color="Payment", barmode="group")
+fig2 = data["Are you currently attending University / College?"].value_counts().plot(kind="pie")
 app.layout = html.Div(children=[
     html.H1('Course Dashboard'),
     dcc.Graph(
-        id='example-graph',
+        id='graph1',
         figure=fig
+    ), 
+    dcc.Graph(
+        id='graph1',
+        figure=fig2
     )
+
 ])
 
 if __name__ == '__main__':
